be2e8912132de70f1d4a9b75a7794e74b08f959d
tinku <gtinku443@gmail.com>
2026-05-23 22:24:34 +0530

    updated app.py

--- user-service/app.py
+++ user-service/app.py
@@ -1,31 +1,14 @@
 from flask import Flask
-import oracledb
-oracledb.init_oracle_client(
-    lib_dir=r"C:\oracle\instantclient-basic-windows.x64-23.26.1.0.0\instantclient_23_0"
-)
 
 app = Flask(__name__)
 
-connection = oracledb.connect(
-    user="system",
-    password="oracle123",
-    dsn="localhost/XE"
-)
-
 @app.route('/')
 def home():
-    return "User Service Running"
+    return "Docker Flask Running"
 
 @app.route('/users')
 def users():
-
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT * FROM users")
-
-    rows = cursor.fetchall()
-
-    return str(rows)
+    return "Users API Running"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
