4163ed94e4e58ddba314e5d49ce12f79e24a8f59
tinku <gtinku443@gmail.com>
2026-05-28 22:46:14 +0530

    Modified Terraform-kubernetes code

--- terraform/main1.tf
+++ terraform/main1.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.32.0"
-    }
-  }
-}
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
 
 resource "kubernetes_deployment" "user_service" {
   metadata {
