89a68fbeeb8a2cceb53c27d24bd6d5ce56949567
tinku <gtinku443@gmail.com>
2026-05-28 22:04:17 +0530

    Added Terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "banking_user_image" {
+  name = "banking-user-service:v1"
+}
+
+resource "docker_container" "banking_user_container" {
+  name  = "banking-user-container"
+  image = docker_image.banking_user_image.image_id
+
+  ports {
+    internal = 5000
+    external = 5001
+  }
+}
